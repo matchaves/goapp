ed1dedd3e343b0a1fddac186ea5cdb28a296b8ec
Your Name <you@example.com>
2021-06-01 20:32:58 -0300

    ajuste portas

--- main2.go
+++ main2.go
@@ -62,9 +62,9 @@ func Postparams(w http.ResponseWriter, r *http.Request) {
 		}
 		defer r.Body.Close()
 
-		fmt.Printf("Este serviço foi chamado por: %s age %d Param %s\n", tempPlayer.Name, tempPlayer.Age, tempPlayer.Param)
+		fmt.Printf("Este serviço foi chamado por: %s urlParamReq %d Param %s\n", tempPlayer.Name, tempPlayer.Age, tempPlayer.Param)
 		w.Header().Set("Content-Type", "application/json")
-		tempPlayer.Name = "APP1 RESPONDEU"
+		tempPlayer.Name = "APP2 RESPONDEU"
 		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(tempPlayer)
 	} else {
@@ -78,12 +78,12 @@ func PrintEnv(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("FOO:", os.Getenv("FOO"))
 		//fmt.Fprintf(w, os.Getenv("BAR"))
 		var tempPlayer player
-		tempPlayer.Name = "APP1"
-		tempPlayer.Age = 1
+		tempPlayer.Name = "APP2"
+		tempPlayer.Age = 2
 		tempPlayer.Param = "Sucesso pelo metodo GET no /"
 		fmt.Printf("Got %s age %d Param %s\n", tempPlayer.Name, tempPlayer.Age, tempPlayer.Param)
 		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode("GET on / APP1")
+		json.NewEncoder(w).Encode("GET on / APP2")
 
 	} else {
 		w.WriteHeader(http.StatusMethodNotAllowed)
@@ -95,18 +95,18 @@ func PrintEnv(w http.ResponseWriter, r *http.Request) {
 func MakeRequest(w http.ResponseWriter, r *http.Request) {
 	pathParams := mux.Vars(r)
 	if r.Method == "GET" {
-		fmt.Println(os.Getenv("FOO"))
-		//fmt.Println("UrlAPP1:", os.Getenv("FOO"))
+		fmt.Printf("UrlApp1: %s", os.Getenv("FOO"))
+		//fmt.Println("UrlApp2:", os.Getenv("FOO"))
 		//fmt.Fprintf(w, os.Getenv("BAR"))
 		url := os.Getenv("FOO")
 		val := pathParams["app"]
 		var tempPlayer player
-		tempPlayer.Name = "APP1"
+		tempPlayer.Name = val
 		tempPlayer.Age = 1
 		tempPlayer.Param = "request do app1"
 		jsonData, _ := json.Marshal(tempPlayer)
-		fmt.Println("variavel recebida pelo pathParam:", val)
-		fmt.Println("executando uma request para a variavel do configmap: ", url)
+		fmt.Println("variavel recebida pelo pathParam: %s", val)
+		fmt.Println("executando uma request para a variavel do configmap: %s", os.Getenv("FOO"))
 		request, _ := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
 		request.Header.Set("Content-Type", "application/json; charset=UTF-8")
 		client := &http.Client{}
@@ -151,5 +151,5 @@ func main() {
 	r.HandleFunc("/to/{app}", MakeRequest)
 
 	//http.ListenAndServe(":80", nil)
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":32002", r))
 }
